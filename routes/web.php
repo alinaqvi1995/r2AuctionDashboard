355d324c64bd0170eeb6d4d07d5bb431abd373f2
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-03-30 03:41:51 +0500

    a

--- routes/web.php
+++ routes/web.php
@@ -1,154 +1,97 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ColorController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\RoleController;
-use App\Http\Controllers\PermissionsController;
-use App\Http\Controllers\RolePermissionController;
-use App\Http\Controllers\VendorController;
-use App\Http\Controllers\SubVendorController;
-use App\Http\Controllers\PackageController;
+use App\Http\Controllers\CapacityController;
+use App\Http\Controllers\ManufacturerController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\CarrierController;
+use App\Http\Controllers\RegionController;
+use App\Http\Controllers\ModelNumberController;
+use App\Http\Controllers\LockStatusController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\SubcategoryController;
-use App\Http\Controllers\TicketController;
-use App\Http\Controllers\VariantController;
-use App\Http\Controllers\UserManagementController;
-use App\Http\Controllers\BlogsController;
-use App\Http\Controllers\Vendor\BlogController;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
+    // return view('login');
     return redirect()->route('login');
 });
 
-Route::get('/unauthorized', function () {
-    return view('unauthorized');
-})->name('unauthorized');
-
-Auth::routes();
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-
-// Admin Routes
-Route::middleware('admin')->prefix('admin')->group(function () {
-
-    Route::get('/', [AdminController::class, 'index'])->name('admin');
-
-    // Roles Crud
-    Route::resource('roles', RoleController::class);
-
-    // Permissions Crud
-    Route::resource('permissions', PermissionsController::class);
-
-    // RolesPermissions Crud
-    Route::resource('role_permissions', RolePermissionController::class);
-    Route::post('/get_permissions', [RolePermissionController::class, 'getPermissions'])->name('get_permissions_by_role');
-
-    // Packages Crud
-    Route::resource('packages', PackageController::class);
-
-    // Categories Crud
-    Route::resource('categories', CategoryController::class);
-
-    // Products Crud
-    Route::resource('products', ProductController::class);
-
-    // Events Crud
-    Route::resource('events', EventController::class);
-
-    // Subcategories Crud
-    Route::resource('subcategories', SubcategoryController::class);
-
-    // Tickets Crud
-    Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
-    Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
-    Route::get('/tickets/{ticket}/edit', [TicketController::class, 'edit'])->name('tickets.edit');
-    Route::put('/tickets/{ticket}', [TicketController::class, 'update'])->name('tickets.update');
-    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
-    
-    // get categories
-    Route::get('/get-categories/{packageId}', [EventController::class, 'getCategories'])->name('get.categories');
-    
-    // get subcategories
-    Route::get('/get-subcategories/{categoryId}', [ProductController::class, 'getsubCategories'])->name('get.subcategories');
-    
-    // user management
-    Route::get('/users', [UserManagementController::class, 'indexUser'])->name('users.index');
-    Route::get('/users/new', [UserManagementController::class, 'createUser'])->name('users.add');
-    Route::post('/users/register', [UserManagementController::class, 'register'])->name('users.register');
-    Route::get('/vendor-categories', [UserManagementController::class, 'getCategories'])->name('users.get.categories');
-
-    //blogs
-    Route::resource('blogs', BlogsController::class);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.pages.index');
+    })->name('dashboard');
 });
 
+// Colors
+Route::get('/colors', [ColorController::class, 'index'])->name('colors.index');
+Route::post('/colors', [ColorController::class, 'store'])->name('colors.store');
+Route::get('/colors/{color}/edit', [ColorController::class, 'edit']);
+Route::put('/colors/{color}', [ColorController::class, 'update'])->name('colors.update');
+Route::delete('/colors/{color}', [ColorController::class, 'destroy'])->name('colors.destroy');
 
-// Vendor Routes
-Route::middleware('vendor')->prefix('vendor')->group(function () {
+// Capacity
+Route::resource('capacities', CapacityController::class);
 
-    Route::get('/', [VendorController::class, 'index'])->name('vendor');
+// Manufacturer
+Route::get('manufacturers', [ManufacturerController::class, 'index'])->name('manufacturers.index');
+Route::post('manufacturers', [ManufacturerController::class, 'store'])->name('manufacturers.store');
+Route::get('manufacturers/{manufacturer}/edit', [ManufacturerController::class, 'edit'])->name('manufacturers.edit');
+Route::put('manufacturers/{manufacturer}', [ManufacturerController::class, 'update'])->name('manufacturers.update');
+Route::delete('manufacturers/{manufacturer}', [ManufacturerController::class, 'destroy'])->name('manufacturers.destroy');
 
-    // products
-    Route::get('/products', [VendorController::class, 'indexProduct'])->name('vendor.products');
-    Route::get('/products/edit/{id}', [VendorController::class, 'editProduct'])->name('vendor.products.edit');
-    Route::post('/products/store', [VendorController::class, 'storeProduct'])->name('vendor.products.store');
-    Route::put('/products/update/', [VendorController::class, 'updateProduct'])->name('vendor.products.update');
+// Category
+Route::get('categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::post('categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::get('categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+Route::put('categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+Route::delete('categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
-    // events
-    Route::get('/events', [VendorController::class, 'indexEvent'])->name('vendor.events');
-    Route::get('/events/edit/{event}', [VendorController::class, 'editEvent'])->name('vendor.events.edit');
-    Route::post('/events/store', [VendorController::class, 'storeEvent'])->name('vendor.events.store');
-    Route::put('/events/update/{event}', [VendorController::class, 'updateEvent'])->name('vendor.events.update');
-    
-    // get subcategories
-    Route::get('/get-subcategory/{categoryId}', [ProductController::class, 'getsubCategories'])->name('vendor.get.subcategories');
-    
-    //blogs
-    Route::get('/blogs', [BlogController::class, 'index'])->name('vendor.blogs.index');
-    Route::get('/blogs/add', [BlogController::class, 'create'])->name('vendor.blogs.create');
-    Route::post('/blogs', [BlogController::class, 'store'])->name('vendor.blogs.store');
-    Route::get('/blogs/{blog}/edit', [BlogController::class, 'edit'])->name('vendor.blogs.edit');
-    Route::put('/blogs/{blog}', [BlogController::class, 'update'])->name('vendor.blogs.update');
-    Route::delete('/blogs/{blog}', [BlogController::class, 'destroy'])->name('vendor.blogs.destroy');
-});
+// Grade
+Route::get('grades', [GradeController::class, 'index'])->name('grades.index');
+Route::post('grades', [GradeController::class, 'store'])->name('grades.store');
+Route::get('grades/{grade}/edit', [GradeController::class, 'edit'])->name('grades.edit');
+Route::put('grades/{grade}', [GradeController::class, 'update'])->name('grades.update');
+Route::delete('grades/{grade}', [GradeController::class, 'destroy'])->name('grades.destroy');
 
+// Carrier
+Route::get('carriers', [CarrierController::class, 'index'])->name('carriers.index');
+Route::post('carriers', [CarrierController::class, 'store'])->name('carriers.store');
+Route::get('carriers/{carrier}/edit', [CarrierController::class, 'edit'])->name('carriers.edit');
+Route::put('carriers/{carrier}', [CarrierController::class, 'update'])->name('carriers.update');
+Route::delete('carriers/{carrier}', [CarrierController::class, 'destroy'])->name('carriers.destroy');
 
-// SubVendor Routes
-Route::middleware('subVendor')->prefix('subVendor')->group(function () {
+// Region
+Route::get('regions', [RegionController::class, 'index'])->name('regions.index');
+Route::post('regions', [RegionController::class, 'store'])->name('regions.store');
+Route::get('regions/{region}/edit', [RegionController::class, 'edit'])->name('regions.edit');
+Route::put('regions/{region}', [RegionController::class, 'update'])->name('regions.update');
+Route::delete('regions/{region}', [RegionController::class, 'destroy'])->name('regions.destroy');
 
-    Route::get('/', [SubVendorController::class, 'index'])->name('subVendor');
+// Model Numbers
+Route::get('model_numbers', [ModelNumberController::class, 'index'])->name('modelnumbers.index');
+Route::post('model_numbers', [ModelNumberController::class, 'store'])->name('modelnumbers.store');
+Route::get('model_numbers/{modelnumber}/edit', [ModelNumberController::class, 'edit'])->name('modelnumbers.edit');
+Route::put('model_numbers/{modelnumber}', [ModelNumberController::class, 'update'])->name('modelnumbers.update');
+Route::delete('model_numbers/{modelnumber}', [ModelNumberController::class, 'destroy'])->name('modelnumbers.destroy');
 
-    // products
-    Route::get('/my_products', [SubVendorController::class, 'indexProducts'])->name('subVendor.products');
-    Route::get('/my_products/edit/{id}', [SubVendorController::class, 'editProducts'])->name('subVendor.products.edit');
-    Route::post('/my_products/store', [SubVendorController::class, 'storeProducts'])->name('subVendor.products.store');
-    Route::put('/my_products/update/', [SubVendorController::class, 'updateProducts'])->name('subVendor.products.update');
+// Model Numbers
+Route::get('lock_statuses', [LockStatusController::class, 'index'])->name('lockstatuses.index');
+Route::post('lock_statuses', [LockStatusController::class, 'store'])->name('lockstatuses.store');
+Route::get('lock_statuses/{lockStatus}/edit', [LockStatusController::class, 'edit'])->name('lockstatuses.edit');
+Route::put('lock_statuses/{lockStatus}', [LockStatusController::class, 'update'])->name('lockstatuses.update');
+Route::delete('lock_statuses/{lockStatus}', [LockStatusController::class, 'destroy'])->name('lockstatuses.destroy');
 
-    // events
-    Route::get('/my_events', [SubVendorController::class, 'indexEvents'])->name('subVendor.events');
-    Route::get('/my_events/edit/{event}', [SubVendorController::class, 'editEvents'])->name('subVendor.events.edit');
-    Route::post('/my_events/store', [SubVendorController::class, 'storeEvents'])->name('subVendor.events.store');
-    Route::put('/my_events/update/{event}', [SubVendorController::class, 'updateEvents'])->name('subVendor.events.update');
-
-    // blogs
-    // Route::get('/my_events', [SubVendorController::class, 'indexEvents'])->name('subVendor.events');
-    // Route::get('/my_events/edit/{event}', [SubVendorController::class, 'editEvents'])->name('subVendor.events.edit');
-    // Route::post('/my_events/store', [SubVendorController::class, 'storeEvents'])->name('subVendor.events.store');
-    // Route::put('/my_events/update/{event}', [SubVendorController::class, 'updateEvents'])->name('subVendor.events.update');
-
-});
+// Products
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::post('products', [ProductController::class, 'store'])->name('products.store');
+Route::get('products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
