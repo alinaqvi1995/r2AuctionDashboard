3b3a6a680075189a61641ecfa4288514c450c0a3
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-04-19 05:11:58 +0500

    a

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,6 +21,8 @@ class Product extends Model
         'image',
     ];
     
+    protected $dates = ['deleted_at', 'created_at', 'updated_at'];
+
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id','id');
