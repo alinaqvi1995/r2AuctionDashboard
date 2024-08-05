04a9ddd845f6647a84828941559948df66416603
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-08-05 13:16:15 +0500

    s

--- routes/api.php
+++ routes/api.php
@@ -62,7 +62,7 @@ Route::prefix('users')->group(function () {
     Route::get('/', [UserApiController::class, 'index']);
     Route::get('/{id}', [UserApiController::class, 'show']);
     Route::post('/', [UserApiController::class, 'store']);
-    Route::put('/{id}', [UserApiController::class, 'update']);
+    Route::post('/{id}', [UserApiController::class, 'update']);
     Route::delete('/{id}', [UserApiController::class, 'destroy']);
 });
 
