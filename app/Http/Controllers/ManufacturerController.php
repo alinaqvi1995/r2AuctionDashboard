83f7a1d1579b053557e75a4201c7af6b4bf8630e
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-08-20 10:11:32 +0500

    s

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -29,7 +29,7 @@ class ManufacturerController extends Controller
             $filenamenew = date('Y-m-d') . "_." . $numb . "_." . $extension;
             $filenamepath = 'manufacturer_icons' . '/' . 'img/' . $filenamenew;
             $filename = $img->move(public_path('storage/manufacturer_icons' . '/' . 'img'), $filenamenew);
-            $iconPath = url('/') . $filenamepath;
+            $iconPath = url('/') . '/' . 'storage' . '/' . $filenamepath;
         }
 
         $manufacturer = Manufacturer::create([
@@ -80,7 +80,7 @@ class ManufacturerController extends Controller
                 $filenamepath = 'manufacturer_icons' . '/' . 'img/' . $filenamenew;
                 $filename = $img->move(public_path('storage/manufacturer_icons' . '/' . 'img'), $filenamenew);
                 $iconPath = $filenamepath;
-                $data['icon'] = url('/') . $iconPath;
+                $iconPath = url('/') . '/' . 'storage' . '/' . $filenamepath;
             }
 
             $manufacturer->update($data);
