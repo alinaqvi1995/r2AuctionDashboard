f03177696857ee4bb01b4cc7b5478830780415b0
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-08-29 04:42:53 +0500

    s

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('orders:create-from-bids')->everyMinute();
+        $schedule->command('your:command')->everyMinute()->withoutOverlapping();
     }
 
     /**
