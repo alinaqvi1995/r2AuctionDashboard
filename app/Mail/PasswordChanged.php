ea44d3d1dc88a25dd4fb7529d3699f2620b2c74e
alinaqvi1995 <53695818+alinaqvi1995@users.noreply.github.com>
2024-09-12 08:44:56 +0500

    s

--- app/Mail/PasswordChanged.php
+++ app/Mail/PasswordChanged.php
@@ -12,28 +12,17 @@ class PasswordChanged extends Mailable
 
     public $userName;
 
-    /**
-     * Create a new message instance.
-     *
-     * @param string $userName
-     * @return void
-     */
     public function __construct($userName)
     {
         $this->userName = $userName;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
         return $this->subject('Your Password Has Been Changed!')
-                    ->view('emails.password_changed')
-                    ->with([
-                        'userName' => $this->userName,
-                    ]);
+            ->view('emails.password_changed')
+            ->with([
+                'userName' => $this->userName,
+            ]);
     }
 }
